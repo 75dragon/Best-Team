d3c05980409c7cea442e632e85a1d161c2519b91
Austin Cheng <austinbicheng@gmail.com>
2017-06-12 14:40:55 -0700

    a bit of main

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import HW5.HashQP;
+import HW5.HashSC;
+import HW5.IpAddress;
+/**
+ * 
+ * @author Austin Cheng
+ *
+ */
+public class Main
+{
+	public Scanner userScanner = new Scanner(System.in);
+	
+	public Main()
+	{
+		HashMap<Country, ColorVertex<Country>> theMap;
+		fillTable(theMap);
+	}
+	
+	public Scanner openInputFile()
+	{
+		String filename;
+		Scanner scanner = null;
+
+		System.out.print("Enter the input filename: ");
+		filename = userScanner.nextLine();
+		File file = new File(filename);
+
+		try
+		{
+			scanner = new Scanner(file);
+		} // end try
+		catch (FileNotFoundException fe)
+		{
+			System.out.println("Can't open input file\n");
+			return null; // array of 0 elements
+		} // end catch
+		return scanner;
+	}
+	
+	public void fillTable(HashMap<Country, ColorVertex<Country>> hold1)
+	{
+		Scanner bob = openInputFile();
+		String hold;
+		Country holdIP = null;
+		if (bob == null)
+		{
+			return;
+		}
+		while (bob.hasNextLine())
+		{
+			hold = bob.nextLine().trim();
+			holdIP = new Country(hold);
+			System.out.println("Inserted into HashSC: " + holdIP.getDottedDecimal() + ", " + holdIP.getIpValue());
+			hold1.insert(holdIP);
+			System.out.println("Inserted into HashQP: " + holdIP.getDottedDecimal() + ", " + holdIP.getIpValue());
+			hold2.insert(holdIP);
+		}
+		return holdIP;
+	}
+	
+	public static void main( String args[] )
+	{
+		new Main();
+	}
+}
---
+++ src/README.md
@@ -0,0 +1,19 @@
+# graph_project_tasks
+
+•	a subclass of the Graph class which has
+-	addEdge method
+-	removeEdge method
+-	undo method
+-	displayGraph method
+
+•	a data class for the states/provinces of a country
+
+•	a problem* solving class which has a save_as_file method
+*(determine if you could color a map using a particular number of colors, OR find the minimum number of colors needed)
+**(algorithms needs to be found and implement)
+
+•	a driver class which has the menu
+
+•	around 3 sets of data in files with fixed format
+
+•	the design of menu and output format
